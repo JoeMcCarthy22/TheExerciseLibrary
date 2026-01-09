e0a6054a26b924b28cce4f124e529b35945dcf1f
JoeMcCarthy22 <Joe_McCarthy22@hotmail.co.uk>
2026-01-09 16:37:07 +0000

    db

--- config/database.js
+++ config/database.js
@@ -2,19 +2,23 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://joemccarthy22:Chelsea1905@project-cluster.wp7nf.mongodb.net/ExerciseLibrary?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error("MongoDB connection error:", err);
+    console.error(err);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
 
 
 
