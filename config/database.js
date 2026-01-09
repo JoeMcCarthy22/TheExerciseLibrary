dfe844f90cf863c0367bededfc6cd4be862a4b47
JoeMcCarthy22 <Joe_McCarthy22@hotmail.co.uk>
2026-01-09 16:16:46 +0000

    updated database.js

--- config/database.js
+++ config/database.js
@@ -5,6 +5,25 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+
+/* const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
     });
@@ -18,4 +37,4 @@ const connectDB = async () => {
 
 
 
-module.exports = connectDB;
+module.exports = connectDB; */
