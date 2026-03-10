4d2446c40f7bd55d80a89b4c26bcc758d65e6a94
JoeMcCarthy22 <Joe_McCarthy22@hotmail.co.uk>
2026-03-10 17:22:05 +0000

    finalised guest login button

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,11 @@
 const passport = require("passport");
 const validator = require("validator");
-const User = require("../models/User");
+const User = require("../models/User"); // only once at the top
 
+// ======= LOGIN =======
 exports.getLogin = (req, res) => {
-  if (req.user) {
-    return res.redirect("/profile");
-  }
-  res.render("login", {
-    title: "Login",
-  });
+  if (req.user) return res.redirect("/profile");
+  res.render("login", { title: "Login" });
 };
 
 exports.postLogin = (req, res, next) => {
@@ -22,47 +19,37 @@ exports.postLogin = (req, res, next) => {
     req.flash("errors", validationErrors);
     return res.redirect("/login");
   }
-  req.body.email = validator.normalizeEmail(req.body.email, {
-    gmail_remove_dots: false,
-  });
+
+  req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
 
   passport.authenticate("local", (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
+    if (err) return next(err);
     if (!user) {
       req.flash("errors", info);
       return res.redirect("/login");
     }
     req.logIn(user, (err) => {
-      if (err) {
-        return next(err);
-      }
+      if (err) return next(err);
       req.flash("success", { msg: "Success! You are logged in." });
       res.redirect(req.session.returnTo || "/feed");
     });
   })(req, res, next);
 };
 
+// ======= LOGOUT =======
 exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log('User has logged out.')
-  })
+  req.logout(() => {});
   req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
+    if (err) console.log("Failed to destroy session during logout.", err);
     req.user = null;
     res.redirect("/");
   });
 };
 
+// ======= SIGNUP =======
 exports.getSignup = (req, res) => {
-  if (req.user) {
-    return res.redirect("/profile");
-  }
-  res.render("signup", {
-    title: "Create Account",
-  });
+  if (req.user) return res.redirect("/profile");
+  res.render("signup", { title: "Create Account" });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -70,19 +57,16 @@ exports.postSignup = (req, res, next) => {
   if (!validator.isEmail(req.body.email))
     validationErrors.push({ msg: "Please enter a valid email address." });
   if (!validator.isLength(req.body.password, { min: 8 }))
-    validationErrors.push({
-      msg: "Password must be at least 8 characters long",
-    });
+    validationErrors.push({ msg: "Password must be at least 8 characters long" });
   if (req.body.password !== req.body.confirmPassword)
     validationErrors.push({ msg: "Passwords do not match" });
 
   if (validationErrors.length) {
     req.flash("errors", validationErrors);
-    return res.redirect("../signup");
+    return res.redirect("/signup");
   }
-  req.body.email = validator.normalizeEmail(req.body.email, {
-    gmail_remove_dots: false,
-  });
+
+  req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
 
   const user = new User({
     userName: req.body.userName,
@@ -93,23 +77,15 @@ exports.postSignup = (req, res, next) => {
   User.findOne(
     { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
     (err, existingUser) => {
-      if (err) {
-        return next(err);
-      }
+      if (err) return next(err);
       if (existingUser) {
-        req.flash("errors", {
-          msg: "Account with that email address or username already exists.",
-        });
-        return res.redirect("../signup");
+        req.flash("errors", { msg: "Account with that email or username already exists." });
+        return res.redirect("/signup");
       }
       user.save((err) => {
-        if (err) {
-          return next(err);
-        }
+        if (err) return next(err);
         req.logIn(user, (err) => {
-          if (err) {
-            return next(err);
-          }
+          if (err) return next(err);
           res.redirect("/profile");
         });
       });
@@ -117,13 +93,21 @@ exports.postSignup = (req, res, next) => {
   );
 };
 
-const handleGuestLogin = async () => {
+// ======= GUEST LOGIN =======
+
+exports.getGuestLogin = async (req, res, next) => {
   try {
-    await login({
-      email: "Test@Test.com",
-      password: "test1234"
+    const guest = await User.findOne({ email: "test@test.com" });
+
+    if (!guest) {
+      return res.status(404).send("Guest user not found");
+    }
+
+    req.logIn(guest, (err) => {
+      if (err) return next(err);
+      res.redirect("/profile");
     });
-  } catch (error) {
-    console.error("Guest login failed:", error);
+  } catch (err) {
+    next(err);
   }
 };
--- routes/main.js
+++ routes/main.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const router = express.Router();
+
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
-const feedController = require("../controllers/feed")
+const feedController = require("../controllers/feed");
 const { ensureAuth } = require("../middleware/auth");
 
-//Main Routes 
+// ======= MAIN PAGES =======
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
-router.get("/feed", ensureAuth, feedController.getFeed)
+router.get("/feed", ensureAuth, feedController.getFeed);
 
-//Routes for user login/signup
+// ======= AUTH ROUTES =======
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
+router.get("/guest-login", authController.getGuestLogin); // <-- guest login
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
